a7c3ec4cb9a5f922ba160638a4a3f7b688ab943f
Matias Sanchez Villar <matuite96@gmail.com>
2022-06-24 00:57:38 -0300

    Relaciones de las clases clientes y prestamos listas

--- tp_integrador_grupo3/src/main/resources/dominio/Prestamo.java
+++ tp_integrador_grupo3/src/main/resources/dominio/Prestamo.java
@@ -4,9 +4,12 @@ import java.io.Serializable;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
 @Entity
 public class Prestamo implements Serializable {
@@ -16,13 +19,57 @@ public class Prestamo implements Serializable {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 	
+	@ManyToOne(cascade= {CascadeType.ALL}, fetch = FetchType.EAGER)
+	@JoinColumn(name="idBiblioteca")
 	private Biblioteca biblioteca;
-	private date fechaPrestamo;
+	
+	private Date fechaPrestamo;
 	private int cantidadDias;
 	
-	@OneToOne(cascade = {CascadeType.ALL})
-	@JoinColumn(name = "cliente_id")
+	@ManyToOne(cascade= {CascadeType.ALL}, fetch = FetchType.EAGER)
+	@JoinColumn(name="idCliente")
 	private Clientes cliente;
 	
+	public Prestamo() {}
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public Biblioteca getBiblioteca() {
+		return biblioteca;
+	}
+
+	public void setBiblioteca(Biblioteca biblioteca) {
+		this.biblioteca = biblioteca;
+	}
+	
+	public Date getFechaPrestamo() {
+		return fechaPrestamo;
+	}
+
+	public void setFechaPrestamo(Date fechaPrestamo) {
+		this.fechaPrestamo = fechaPrestamo;
+	}
+	
+	public int getCantidadDias() {
+		return cantidadDias;
+	}
+
+	public void setCantidadDias(int cantidadDias) {
+		this.cantidadDias = cantidadDias;
+	}
+	
+	public Clientes getCantidadDias() {
+		return cliente;
+	}
+
+	public void setClientes(Clientes cliente) {
+		this.cliente = cliente;
+	}
 	
 }
